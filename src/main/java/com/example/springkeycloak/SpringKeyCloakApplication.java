74f4d213fc3b8332e15dc584e3848c0be951c301
AstikJun <serikovastik@gmail.com>
2023-08-15 16:29:20 +0600

    Init

---
+++ src/main/java/com/example/springkeycloak/SpringKeyCloakApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeyCloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKeyCloakApplication.class, args);
+    }
+
+}
